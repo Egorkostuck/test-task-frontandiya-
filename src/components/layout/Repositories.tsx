f49b67c3c4f554ee31b96d35d3f9351c67c81a16
Egor Kostuck <turak_09@mail.ru>
2024-01-15 22:58:27 +0300

    create pagination, refactor api request, change logic for repositories request, add adaptive layout. minor refactor code

--- src/components/layout/Repositories.tsx
+++ src/components/layout/Repositories.tsx
@@ -2,6 +2,7 @@ import { FC, JSX } from 'react';
 
 import { useSelector } from 'react-redux';
 
+import PaginationApp from '../shared/Pagination';
 import RepoCards from '../shared/RepoCards';
 
 import '../../styles/components/repositories.scss';
@@ -14,25 +15,48 @@ interface Repo {
 }
 
 const Repositories: FC = (): JSX.Element => {
-  const { repositories } = useSelector((state: any) => state.user);
+  const { user, repositories, currentPage } = useSelector((state: any) => state.user);
+  const itemsPerPage: number = 4;
+
+  const getRepoItems = (): JSX.Element | undefined => {
+    if (!repositories.length) return;
+
+    return (
+      <>
+        {repositories.map((repo: Repo) => {
+          return (
+            <RepoCards
+              key={repo.id}
+              name={repo.name}
+              description={repo.description}
+              url={repo.html_url}
+            />
+          );
+        })}
+      </>
+    );
+  };
+
+  const getPagination = (): JSX.Element | undefined => {
+    if (!repositories.length) return;
+
+    return (
+      <PaginationApp
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage}
+        countItems={user.public_repos}
+      />
+    );
+  };
 
   return (
     <div className="repositories">
       <div className="repositories__content">
-        <h3 className="repositories__title">{`Repositories (${repositories.length})`}</h3>
+        <h3 className="repositories__title">{`Repositories (${user.public_repos})`}</h3>
 
-        <div className="cards__container">
-          {repositories.map((repo: Repo) => {
-            return (
-              <RepoCards
-                key={repo.id}
-                name={repo.name}
-                description={repo.description}
-                url={repo.html_url}
-              />
-            );
-          })}
-        </div>
+        <div className="cards__container">{getRepoItems()}</div>
+
+        {getPagination()}
       </div>
     </div>
   );
